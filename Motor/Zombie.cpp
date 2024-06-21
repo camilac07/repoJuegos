7b6e1ed39a5acd92d5a5c23d7800a0cdbfb42ef0
camilac07 <camilacondeisla007@gmail.com>
2024-06-21 17:26:09 -0500

    ta4

--- Motor/Zombie.cpp
+++ Motor/Zombie.cpp
@@ -16,7 +16,13 @@ void Zombie::init(float speed, glm::vec2 position)
 	this->speed = speed;
 	this->position = position;
 	color.set(24, 74, 2, 0.8);
-
+	this->position = position;
+	std::mt19937 randomEngine(time(nullptr));
+	std::uniform_real_distribution<float>randDir(1.0f, 1.0f);
+	direction = glm::vec2(randDir(randomEngine), randDir(randomEngine));
+	if (direction.length() == 0) {
+		direction = glm::vec2(1.0f, 1.0f);
+	}
 
 }
 
@@ -24,6 +30,7 @@ void Zombie::update(vector<string>& levelData, vector<Human*>& humans, vector<Zo
 {
 	std::mt19937 randomEngine(time(nullptr));
 	std::uniform_real_distribution<float>randRotate(-40.0f, 40.0f);
+	position += direction * speed;
 	if (collideWithLevel(levelData)) {
 		direction = glm::rotate(direction, randRotate(randomEngine));
 	}
